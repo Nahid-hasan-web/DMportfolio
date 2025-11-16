23e5a9865fc69623255f1e0ab16d670729ccb2d1
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-16 15:16:01 +0600

    work process added and about me done

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -4,6 +4,8 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaBehance } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa6";
+import ButtonV1 from "../common-components/ButtonV1";
+import { GoDownload } from "react-icons/go";
 
 const AboutMe = () => {
   const socialLiks = [
@@ -16,7 +18,7 @@ const AboutMe = () => {
     <>
       <section id="AboutMe" className="py-5 absolute z-20 w-full -bottom-[10%]  ">
         <div className="container">
-          <div className="p-28 bg-white rounded-4 shadow-[0px_10px_20px_0px_rgba(0,0,0,0.15)]  ">
+          <div className="p-28 bg-white rounded-4 flex justify-between  shadow-[0px_10px_20px_0px_rgba(0,0,0,0.15)]  ">
           {/* --- about me images */}
             <div className="w-[424px] h-[468px]  relative bg-gray-100 ">
               <div className=" absolute -bottom-9 left-[50%] translate-x-[-50%] w-[264px] h-[72px]  bg-white rounded-sm p-3 flex justify-between items-center shadow-[0px_8px_16px_-2px_rgba(0,_0,_0,_0.1)]">
@@ -33,13 +35,19 @@ const AboutMe = () => {
             </div>
             {/* ---- about me text */}
             <div>
-                <h2 className="text-[38px] font-semibold font-work text-[#132238]">I am Professional User Experience Designer</h2>
-                <p className="commonP max-w-[536px] leading-[24px] ">
+                <h2 className="w-[426px] text-[38px] font-semibold font-work text-[#132238]">I am Professional User Experience Designer</h2>
+                <p className=" w-[536px] mt-8 max-w-[536px] leading-6 text-[#556070]">
                     I design and develop services for customers specializing creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences.
                 </p>
-                <p className="commonP max-w-[536px] leading-[24px] ">
+                <p className=" w-[536px] mt-4 max-w-[536px] leading-6 text-[#556070] ">
                     I design and develop services for customers specializing creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences.
                 </p>
+                <div className="flex items-center gap-6 mt-8">
+                    <ButtonV1 buttonContent={'My Project'}/>
+                    <a className="border flex items-center gap-3 border-brandColor rounded-1 py-3 px-6 text-base font-work font-semibold text-brandColor">
+                      <GoDownload/>  Download CV
+                    </a>
+                </div>
             </div>
 
           </div>
---
+++ app/components/WorkPorcess.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WorkPorcess = () => {
+  return (
+    <section className='WorkProcess'>
+        <div className="container">
+            <div className="flex justify-between items-center"></div>
+        </div>
+
+    </section>
+  )
+}
+
+export default WorkPorcess
