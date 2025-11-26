60ee094006453441d780be590cef1aa07cfa59c0
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-26 09:26:03 +0600

    blog card and blog details page added

---
+++ app/(pages)/blogDetails/page.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <section id='blogDetails'>
+            <div className="container">
+                <h2 className='text-[60px] font-medium  font-poppins text-black'>Blog Name</h2>
+                <p className='text-base font-normal font-poppins text-gray-600 mt-5'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam quia voluptatum magnam aliquam iusto dolorum sit impedit, cumque magni, minus molestias. Perspiciatis aspernatur voluptatem enim repellat. Rem, illum hic?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci odio ex repellat delectus! Hic aliquid nihil vero rem, aspernatur, illo quidem pariatur voluptate fugit eum accusantium, blanditiis porro repellendus culpa! Consectetur repudiandae dicta magnam officiis vel soluta odio exercitationem! Maxime architecto deleniti nesciunt repellat nemo cumque? Totam rerum nobis pariatur suscipit ipsam minus voluptatum omnis harum quo, non culpa atque excepturi repellendus officia? Animi consectetur quaerat sint reiciendis hic assumenda odio, doloremque reprehenderit ipsam cumque dignissimos atque laboriosam nulla blanditiis necessitatibus. Delectus magnam possimus dolore odit fuga esse aspernatur adipisci distinctio qui aliquid perferendis recusandae mollitia ut vel, consequuntur dolorem.
+                </p>
+                <p className='text-base font-normal font-poppins text-gray-600 mt-5'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam quia voluptatum magnam aliquam iusto dolorum sit impedit, cumque magni, minus molestias. Perspiciatis aspernatur voluptatem enim repellat. Rem, illum hic?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci odio ex repellat delectus! Hic aliquid nihil vero rem, aspernatur, illo quidem pariatur voluptate fugit eum accusantium, blanditiis porro repellendus culpa! Consectetur repudiandae dicta magnam officiis vel soluta odio exercitationem! Maxime architecto deleniti nesciunt repellat nemo cumque? Totam rerum nobis pariatur suscipit ipsam minus voluptatum omnis harum quo, non culpa atque excepturi repellendus officia? Animi consectetur quaerat sint reiciendis hic assumenda odio, doloremque reprehenderit ipsam cumque dignissimos atque laboriosam nulla blanditiis necessitatibus. Delectus magnam possimus dolore odit fuga esse aspernatur adipisci distinctio qui aliquid perferendis recusandae mollitia ut vel, consequuntur dolorem.
+                </p>
+                <p className='text-base font-normal font-poppins text-gray-600 mt-5'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam quia voluptatum magnam aliquam iusto dolorum sit impedit, cumque magni, minus molestias. Perspiciatis aspernatur voluptatem enim repellat. Rem, illum hic?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci odio ex repellat delectus! Hic aliquid nihil vero rem, aspernatur, illo quidem pariatur voluptate fugit eum accusantium, blanditiis porro repellendus culpa! Consectetur repudiandae dicta magnam officiis vel soluta odio exercitationem! Maxime architecto deleniti nesciunt repellat nemo cumque? Totam rerum nobis pariatur suscipit ipsam minus voluptatum omnis harum quo, non culpa atque excepturi repellendus officia? Animi consectetur quaerat sint reiciendis hic assumenda odio, doloremque reprehenderit ipsam cumque dignissimos atque laboriosam nulla blanditiis necessitatibus. Delectus magnam possimus dolore odit fuga esse aspernatur adipisci distinctio qui aliquid perferendis recusandae mollitia ut vel, consequuntur dolorem.
+                </p>
+                <p className='text-base font-normal font-poppins text-gray-600 mt-5'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ullam quia voluptatum magnam aliquam iusto dolorum sit impedit, cumque magni, minus molestias. Perspiciatis aspernatur voluptatem enim repellat. Rem, illum hic?
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci odio ex repellat delectus! Hic aliquid nihil vero rem, aspernatur, illo quidem pariatur voluptate fugit eum accusantium, blanditiis porro repellendus culpa! Consectetur repudiandae dicta magnam officiis vel soluta odio exercitationem! Maxime architecto deleniti nesciunt repellat nemo cumque? Totam rerum nobis pariatur suscipit ipsam minus voluptatum omnis harum quo, non culpa atque excepturi repellendus officia? Animi consectetur quaerat sint reiciendis hic assumenda odio, doloremque reprehenderit ipsam cumque dignissimos atque laboriosam nulla blanditiis necessitatibus. Delectus magnam possimus dolore odit fuga esse aspernatur adipisci distinctio qui aliquid perferendis recusandae mollitia ut vel, consequuntur dolorem.
+                </p>
+
+            </div>
+    </section>
+
+    </>
+  )
+}
+
+export default page
